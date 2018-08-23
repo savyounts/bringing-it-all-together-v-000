56ab7f850c6654fc468d0f688a7b3d0a1209da09
Sav Younts <savyounts@gmail.com>
2018-08-23 21:32:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = self.new(row[0], row[1], row[2])
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
     dog.id = row[0]
     dog.name = row[1]
     dog.breed = row[2]
